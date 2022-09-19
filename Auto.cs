be20daed234c5dfff3fbbc45b34caeb375f9f259
MinoNihou <patriciatorker@gmailcom>
2022-09-20 01:09:10 +0200

    Projektdateien hinzufügen.

---
+++ Auto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenDefinieren
+{
+    public class Auto
+    {
+        public string marke;
+        public int alterInJahren;
+        private int aktuelleGeschwindigkeit;
+
+        public void BeschleunigeAuf100Kmh()
+        {
+            aktuelleGeschwindigkeit = 100;
+
+            if (this.IstSchnell())
+            {
+                Console.WriteLine("Habe schnell beschleunigt <3");
+            }
+            else
+            {
+                Console.WriteLine("Habe normal beschleunigt");
+            }
+        }
+
+        public int SagAktuelleGeschwindigkeit()
+        {
+            return aktuelleGeschwindigkeit;
+        }
+
+        private bool IstSchnell()
+        {
+            if (marke == "Porsche")
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenDefinieren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Auto meinAuto = new Auto();
+            Auto deinAuto = new Auto();
+
+            meinAuto.alterInJahren = 13;
+            meinAuto.marke = "Audi";            
+
+            meinAuto.alterInJahren = 1;
+            meinAuto.marke = "Portsche";
+
+
+            Console.WriteLine("Mein Auto fährt aktuell {0} km/h", meinAuto.SagAktuelleGeschwindigkeit());
+            Console.WriteLine("Dein Auto fährt aktuell {0} km/h", deinAuto.SagAktuelleGeschwindigkeit());
+            meinAuto.BeschleunigeAuf100Kmh();
+            deinAuto.BeschleunigeAuf100Kmh();
+            Console.WriteLine("Mein Auto fährt aktuell {0} km/h", meinAuto.SagAktuelleGeschwindigkeit());
+            Console.WriteLine("Dein Auto fährt aktuell {0} km/h", meinAuto.SagAktuelleGeschwindigkeit());
+            Console.ReadKey();
+
+
+        }
+    }
+}
